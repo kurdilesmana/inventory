2280377e1b65c6f61f31db065ca6967122842686
Unknown <iki.fatuhrohman@gmail.com>
2019-04-09 20:23:07 +0700

    Auto stash before merge of "master" and "origin/master"

--- initial_database.sql
+++ initial_database.sql
@@ -5,6 +5,10 @@ Create Table `users` (
 	`username` VARCHAR(50) NOT NULL UNIQUE,
 	`password` VARCHAR(100) NOT NULL,
 	`role` VARCHAR(1) NOT NULL
+<<<<<<< Updated upstream
 );
 INSERT INTO `users`(`name`, `username`, `password`, `role`) 
 VALUES ('Administrator','administrator','21232f297a57a5a743894a0e4a801fc3',1);
+=======
+) 
+>>>>>>> Stashed changes
