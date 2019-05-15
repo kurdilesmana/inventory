1fab47476014c315df1346e9f86596dd490c8161
Kurdiansyah Lesmana <kurdilesmana@gmail.com>
2019-05-15 21:24:42 +0700

    Update database

--- initial_database.sql
+++ initial_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2019 at 08:20 AM
+-- Generation Time: May 15, 2019 at 03:53 PM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -49,6 +49,14 @@ CREATE TABLE `category` (
   `status` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id_category`, `category_name`, `description`, `status`) VALUES
+(1, 'Baju Muslim', 'Kategori Baju', '1'),
+(2, 'Celana Jeans', 'Celana Gaul', '1');
+
 -- --------------------------------------------------------
 
 --
@@ -71,7 +79,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `name`, `username`, `password`, `role`, `image`, `date_created`, `is_active`) VALUES
-(1, 'Administrator', 'administrator', '21232f297a57a5a743894a0e4a801fc3', '1', 'default.jpg', '0000-00-00', 0);
+(1, 'Administrator', 'administrator', '21232f297a57a5a743894a0e4a801fc3', '1', 'default.jpg', '0000-00-00', 0),
+(2, 'Kurdiansyah Lesmana', 'kurdilesmana', '21232f297a57a5a743894a0e4a801fc3', '2', 'default.jpg', '0000-00-00', 0);
 
 -- --------------------------------------------------------
 
@@ -94,7 +103,9 @@ INSERT INTO `user_access_menu` (`id`, `menu_id`, `role_id`) VALUES
 (2, 2, 1),
 (3, 1, 2),
 (4, 3, 1),
-(5, 4, 1);
+(5, 4, 1),
+(6, 5, 1),
+(7, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -113,7 +124,8 @@ CREATE TABLE `user_header_menu` (
 
 INSERT INTO `user_header_menu` (`id`, `header_menu`) VALUES
 (1, 'Administrator'),
-(2, 'User');
+(2, 'User'),
+(3, 'Menu');
 
 -- --------------------------------------------------------
 
@@ -125,7 +137,7 @@ CREATE TABLE `user_menu` (
   `id` int(11) NOT NULL,
   `header_id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
-  `is_parent` int(1) NOT NULL,
+  `is_parent` int(1) DEFAULT NULL,
   `url` varchar(100) NOT NULL,
   `icon` varchar(100) NOT NULL,
   `is_active` int(1) NOT NULL
@@ -138,8 +150,10 @@ CREATE TABLE `user_menu` (
 INSERT INTO `user_menu` (`id`, `header_id`, `title`, `is_parent`, `url`, `icon`, `is_active`) VALUES
 (1, 1, 'Dashboard', 0, 'dashboard', 'fa fa-dashboard', 1),
 (2, 1, 'Users', 0, 'users', 'fa fa-user', 1),
-(3, 1, 'Category', 0, 'category', 'fa fa-user', 1),
-(4, 1, 'Brands', 0, 'brands', 'fa fa-user', 1);
+(3, 1, 'Category', 0, 'category', 'fa fa-navicon', 1),
+(4, 1, 'Brands', 0, 'brands', 'fa fa-tags', 1),
+(5, 3, 'Menu Management', 0, 'menu', 'fa fa-folder', 1),
+(6, 3, 'Submenu Management', 0, 'menu/submenu', 'fa fa-folder-open', 1);
 
 -- --------------------------------------------------------
 
@@ -222,31 +236,31 @@ ALTER TABLE `brands`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user_access_menu`
 --
 ALTER TABLE `user_access_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user_header_menu`
 --
 ALTER TABLE `user_header_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user_menu`
 --
 ALTER TABLE `user_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user_role`
